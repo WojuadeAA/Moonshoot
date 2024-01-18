be5f116708e48a0a10bf888e8a83d52e5f92a260
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2024-01-18 10:16:34 +0100

    Initial Commit

---
+++ Moonshoot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshoot
+//
+//  Created by Wojuade Abdul Afeez on 18/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshoot/MoonshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshootApp.swift
+//  Moonshoot
+//
+//  Created by Wojuade Abdul Afeez on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
